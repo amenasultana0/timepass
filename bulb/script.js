f51ef57f691a30dd9f84813ee99c977980c26117
AmenaSultana <168581684+amenasultana0@users.noreply.github.com>
2025-05-24 00:20:15 +0530

    Add files via upload

---
+++ bulb/script.js
@@ -0,0 +1,10 @@
+document.querySelector('.SwitchOn').onclick = () => {
+    document.getElementById('image').src = 'bulb_on.png';
+    document.getElementById('msg1').textContent='You are an idiot so just dont try!';
+    document.getElementById('msg2').textContent='';
+}
+document.querySelector('.SwitchOff').onclick = () => {
+    document.getElementById('image').src = 'bulb_off.png';
+    document.getElementById('msg1').textContent='';
+    document.getElementById('msg2').textContent='Trying to feel good by switching it off again? Idiot mav 🙂';
+}
